e9424aa547815f10616cc1e73d4fb4410551779c
nadarthu <nadarthu@students.zhaw.ch>
2026-05-14 19:05:09 +0200

    server add message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course!----! Thulasi DAS IST LN04 teil hier! Heute ist es der LN09 von Thulasi Nadarajah ');
+  res.send('Hello FS2026 DevOps Course!----! Thulasi DAS IST LN04 teil hier! Heute ist es der LN09 von Thulasi Nadarajah--- :))) ');
 });
 
 app.listen(PORT, HOST);
